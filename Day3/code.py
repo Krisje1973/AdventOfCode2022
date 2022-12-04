b7f0e7502bce87b182b19769a4dd9af99c970ea1
Krisje Jordaens <kris.jordaens@esc.be>
2022-12-04 11:02:52 +0100

    Day 4

--- Day3/code.py
+++ Day3/code.py
@@ -18,20 +18,15 @@ def main():
    second_star()        
     
 def first_star():
-    result = 0
-    for line in input:
-        result += prio.index((dict.fromkeys(line[:len(line)//2],0).items() & dict.fromkeys(line[len(line)//2:],0).items()).pop()[0])
-
     print("Result First Star")
-    print(result)
+    print(sum(prio.index((dict.fromkeys(line[:len(line)//2],0).items() & dict.fromkeys(line[len(line)//2:],0).items()).pop()[0]) for line in input))
 
 def second_star():
     result = 0
-    for line in np.array_split(input,len(input)//3):
-        result += prio.index((dict.fromkeys(line[0],0).items() & dict.fromkeys(line[1],0).items() & dict.fromkeys(line[2],0).items()).pop()[0])
    
     print("Result Second Star")
-    print(result)
+    print(sum(prio.index((dict.fromkeys(line[0],0).items() & dict.fromkeys(line[1],0).items() & dict.fromkeys(line[2],0).items()).pop()[0]) 
+            for line in np.array_split(input,len(input)//3)))
 
 if __name__ == '__main__':
     main()
--- Day4/code.py
+++ Day4/code.py
@@ -1,25 +1,36 @@
 import math
+import numpy as np
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from  AOCHelper import * 
+
 input = []
 def readinput():
    global input
-   input = readinput_lines("Day1\input.txt")
+   input = readinput_lines_with_separator("Day4\input.txt", ",")
   
 def main():
    readinput()
-   first_star()
+   #first_star()
    second_star()        
-          
+
 def first_star():
-    if second_star() != "NYI" : return
+    result = 0
+    for arr in np.array_split(input, len(input)//2):
+        pairs=[]
+        for a in arr:
+            for s in a.split("-"):
+                pairs.append(int(s))
+        if (pairs[0] <= pairs[2] and pairs[1] >= pairs[3]) or (pairs[0] >= pairs[2] and pairs[1] <= pairs[3]):
+            result+=1
+
     print("Result First Star")
+    print(result)
 
 def second_star():
-    return "NYI"
-
     print("Result Second Star")
+    # Stupid but fun :)
+    print(sum(list(map(lambda y:len(set(range(int(y[0][0]),int(y[0][1])+1))&set(range(int(y[1][0]),int(y[1][1])+1)))>0,map(lambda x: list(map(lambda s:s.split("-"),x)),np.array_split(input, len(input)//2))))))
 
 if __name__ == '__main__':
     main()
