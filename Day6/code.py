208b451eefc8e144ed3a9669d700f70d37722e6d
Krisje Jordaens <kris.jordaens@esc.be>
2022-12-07 07:06:39 +0100

    Day 6

--- Day6/code.py
+++ Day6/code.py
@@ -1,11 +1,12 @@
 import math
 import os, sys
+from collections import deque,OrderedDict
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from  AOCHelper import * 
 input = []
 def readinput():
    global input
-   input = readinput_lines("Day1\input.txt")
+   input = readinput_as_string("Day6\input.txt")[0]
   
 def main():
    readinput()
@@ -13,13 +14,27 @@ def main():
    second_star()        
           
 def first_star():
-    if second_star() != "NYI" : return
+    sp=deque(input[0:3])
+    for idx,s in enumerate(input[3:]):
+        sp.append(s)
+        if len(set(sp)) == 4:
+            break
+        sp.popleft()
+
     print("Result First Star")
+    print(idx+4)
 
 def second_star():
-    return "NYI"
+    
+    sp=deque(input[0:13])
+    for idx,s in enumerate(input[13:]):
+        sp.append(s)
+        if len(set(sp)) == 14:
+            break
+        sp.popleft()
 
     print("Result Second Star")
-
+    print(idx+14)
+        
 if __name__ == '__main__':
     main()
