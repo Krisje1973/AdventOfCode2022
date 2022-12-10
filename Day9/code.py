f0d1a1529783b5e3ab87dff0d21c231297751b13
Krisje Jordaens <kris.jordaens@esc.be>
2022-12-10 08:08:37 +0100

    Day 9 p2 failed

--- Day9/code.py
+++ Day9/code.py
@@ -5,12 +5,12 @@ from  AOCHelper import *
 input = []
 def readinput():
    global input
-   input = readinput_lines("Day9\input.txt")
+   input = readinput_lines("Day9\input_ex.txt")
   
 def main():
    readinput()
-   first_star()
-   #second_star()        
+   #first_star()
+   second_star()        
           
 def first_star():
     #6318 to high
@@ -49,9 +49,46 @@ def first_star():
     print(len(result)-1)
    
 def second_star():
-    return "NYI"
+    dirs = defaultdict(tuple)
+    dirs["R"] = (1,0)
+    dirs["L"] = (-1,0)
+    dirs["U"] = (0,1)
+    dirs["D"] = (0,-1)
+    result = set()
+    snakes = [(0, 0) for i in range(10)]
+    for line in input:
+        d,c = line.split()
+        
+        for i in range(int(c)):
+            move = dirs[d]
+            snakes[0] = tuple(map(sum,zip(snakes[0],move)))
+            if d=="U":
+                l=1
+            for i,snake in enumerate(snakes[1:]):
+                h,t=snakes[i],snake
+                if h == t:
+                    continue
 
+                s = tuple(a - b for a, b in zip(h, t))
+                
+                if (abs(s[0]),abs(s[1])) == (1, 1):
+                    continue
+
+                if abs(s[1]) == 1 and s[0] == 0:
+                    continue
+
+                if abs(s[0]) == 1 and s[1] == 0:
+                    continue
+
+                if abs(s[0]) == 2:
+                    snakes[i+1] = tuple(map(sum,zip(snakes[i+1],(1,1))))
+                else:
+                    snakes[i+1] = tuple(map(sum,zip(h,(-move[0],-move[1]))))
+
+            result.add(snakes[9])
+            
     print("Result Second Star")
+    print(len(result)-1)
 
 if __name__ == '__main__':
     main()
