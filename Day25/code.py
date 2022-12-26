edc8a695fce0a3eb5b9bba9424fa598106e72d60
Krisje Jordaens <kris.jordaens@esc.be>
2022-12-26 09:56:28 +0100

    Day 25

--- Day25/code.py
+++ Day25/code.py
@@ -5,17 +5,31 @@ from  AOCHelper import *
 input = []
 def readinput():
    global input
-   input = readinput_lines("Day1\input.txt")
+   input = readinput_lines("Day25\input.txt")
   
 def main():
    readinput()
    first_star()
-   second_star()        
+   #second_star()        
           
 def first_star():
-    if second_star() != "NYI" : return
-    print("Result First Star")
+# Instead of using digits four through zero, the digits are 2, 1, 0, minus (written -), and double-minus (written =). Minus is worth -1, and double-minus is worth -2."
+    digits = {"2": 2, "1": 1, "0": 0, "-": -1, "=": -2}
+    total=0
+    for line in input:
+        #111-1==0
+        for idx,i in enumerate(line.strip()[::-1]):            
+            total += digits[i] * pow(5,idx)
 
+    s = ""
+    digits = {v:k for k,v in digits.items()} | { 4:"-", 3:"="}
+    while total:
+        total, ch = divmod(total,5) #
+        total+=ch>2
+        s = digits[ch] + s
+
+    print("Result First Star")
+    print(s)
 def second_star():
     return "NYI"
 
